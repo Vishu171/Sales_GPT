8098423c0aac1fb86bcbe89df5739bf60d6e9614
JadeIDS <vishwajeet.birajdar@jadeglobal>
2023-12-13 17:14:23 +0530

    First commit

---
+++ salesgpt.py
@@ -0,0 +1,149 @@
+import openai
+import re
+import streamlit as st
+
+QUALIFIED_TABLE_NAME = "FINANCIALS.DEMO.IDS"
+TABLE_DESCRIPTION = """
+This table has details of Intelligent Data Solutions, a brand new Business Unit in Jade Global.
+"""
+# This query is optional if running Frosty on your own table, especially a wide table.
+# Since this is a deep table, it's useful to tell Frosty what variables are available.
+# Similarly, if you have a table with semi-structured data (like JSON), it could be used to provide hints on available keys.
+# If altering, you may also need to modify the formatting logic in get_table_context() below.
+METADATA_QUERY = "SELECT HEADING,DESCRIPTION FROM FINANCIALS.DEMO.IDS;"
+
+GEN_SQL = """
+You will be acting as an AI Snowflake SQL Expert named SalesMate.
+Your goal is to give correct, executable sql query to users.
+You will be replying to users who will be confused if you don't respond in the character of SalesMate.
+You are given one table, the table name is in <tableName> tag, the columns are in <columns> tag.
+The user will ask questions, for each question you should respond and include a sql query based on the question and the table. 
+
+{context}
+
+Here are 6 critical rules for the interaction you must abide:
+<rules>
+1. You MUST MUST wrap the generated sql code within ``` sql code markdown in this format e.g
+```sql
+(select 1) union (select 2)
+```
+2. If I don't tell you to find a limited set of results in the sql query or question, you MUST limit the number of responses to 10.
+3. Text / string where clauses must be fuzzy match e.g ilike %keyword%
+4. Make sure to generate a single snowflake sql code, not multiple. 
+5. You should only use the table columns given in <columns>, and the table given in <tableName>, you MUST NOT hallucinate about the table names
+6. DO NOT put numerical at the very front of sql variable.
+</rules>
+
+Don't forget to use "ilike %keyword%" for fuzzy match queries (especially for variable_name column)
+and wrap the generated sql code with ``` sql code markdown in this format e.g:
+```sql
+(select 1) union (select 2)
+```
+
+For each question from the user, make sure to include a query in your response.
+
+Now to get started, please briefly introduce yourself and display below message.
+<message>
+Good Morning, I am SalesMate, your Intelligent Sales virtual Assist. I am an AI Expert and will provide answers to all your queries on Intelligent Data Solutions, a brand new Business Unit in Jade Global. You can ask me any questions on Intelligent Data Solutions and I will try my level best to answer them.
+
+Here are some guidelines to ask me questions on Intelligent Data Solutions, Business Unit - 
+- Keep your query clear and straightforward manner.
+- Avoid overly complex or jargon-filled language.
+- Use a mix of open-ended questions (e.g., "How can I help you today?") and closed-ended questions (e.g., "Yes or no?") based on the situation.
+- Provide multiple-choice options when appropriate to make it easier for me to respond
+- Be polite and empathetic in your questions.
+- Use courteous language and expressions.
+
+So let's get started....
+</message>
+"""
+
+@st.cache_data(show_spinner=False)
+def get_table_context(table_name: str, table_description: str, metadata_query: str = None):
+    table = table_name.split(".")
+    conn = st.experimental_connection("snowpark")
+    columns = conn.query(f"""
+        SELECT COLUMN_NAME, DATA_TYPE FROM {table[0].upper()}.INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA = '{table[1].upper()}' AND TABLE_NAME = '{table[2].upper()}'
+        """,
+    )
+    columns = "\n".join(
+        [
+            f"- **{columns['COLUMN_NAME'][i]}**: {columns['DATA_TYPE'][i]}"
+            for i in range(len(columns["COLUMN_NAME"]))
+        ]
+    )
+    context = f"""
+Here is the table name <tableName> {'.'.join(table)} </tableName>
+
+<tableDescription>{table_description}</tableDescription>
+
+Here are the columns of the {'.'.join(table)}
+
+<columns>\n\n{columns}\n\n</columns>
+    """
+    if metadata_query:
+        metadata = conn.query(metadata_query)
+        metadata = "\n".join(
+            [
+                f"- **{metadata['HEADING'][i]}**: {metadata['DESCRIPTION'][i]}"
+                for i in range(len(metadata["HEADING"]))
+            ]
+        )
+        context = context + f"\n\nAvailable headings by HEADING_NAME:\n\n{metadata}"
+    return context
+
+def get_system_prompt():
+    table_context = get_table_context(
+        table_name=QUALIFIED_TABLE_NAME,
+        table_description=TABLE_DESCRIPTION,
+        metadata_query=METADATA_QUERY
+    )
+    return GEN_SQL.format(context=table_context)
+
+st.title("SalesMate")
+with st.sidebar:
+    st.image('jade_logo.png',width=200)
+
+# Initialize the chat messages history
+openai.api_key = st.secrets.OPENAI_API_KEY
+if "messages" not in st.session_state:
+    # system prompt includes table information, rules, and prompts the LLM to produce
+    # a welcome message to the user.
+    st.session_state.messages = [{"role": "system", "content": get_system_prompt()}]
+
+# Prompt for user input and save
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+# display the existing chat messages
+for message in st.session_state.messages:
+    if message["role"] == "system":
+        continue
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+        if "results" in message:
+            st.dataframe(message["results"])
+
+# If last message is not from assistant, we need to generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        response = ""
+        resp_container = st.empty()
+        for delta in openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+            stream=True,
+        ):
+            response += delta.choices[0].delta.get("content", "")
+            resp_container.markdown(response)
+
+        message = {"role": "assistant", "content": response}
+        # Parse the response for a SQL query and execute if available
+        sql_match = re.search(r"```sql\n(.*)\n```", response, re.DOTALL)
+        if sql_match:
+            sql = sql_match.group(1)
+            conn = st.experimental_connection("snowpark")
+            message["results"] = conn.query(sql)
+            st.dataframe(message["results"])
+        st.session_state.messages.append(message)
