7b4760595230de4ff2e2fae84e1243f84d940a6e
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-12-13 17:40:13 +0530

    Update salesgpt.py

--- salesgpt.py
+++ salesgpt.py
@@ -103,7 +103,7 @@ def get_system_prompt():
 
 st.title("SalesMate")
 with st.sidebar:
-    st.image('assets/salesmategpt.png',width=280)
+    st.image('assets/salesmategpt.png',width=290)
 
 # Initialize the chat messages history
 openai.api_key = st.secrets.OPENAI_API_KEY
